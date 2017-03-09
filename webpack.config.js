4abbebe824ae00b8631763afcf6489730b345ab0
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-03-08 21:12:52 -0500

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var webpack = require('webpack');
-
+var path = require('path');
 module.exports = {
   // This is the "main" file which should include all other modules
   entry: [
@@ -27,16 +27,13 @@ module.exports = {
   },
   module: {
     // Special compilation rules
-
     loaders: [
       {
         // Ask webpack to check: If this file ends with .js, then apply some transforms
         test: /\.js$/,
         // Transform it with babel
         loader: 'babel',
-        resolveLoader: {
-          root: path.join(__dirname, 'node_modules')
-        },
+        resolveLoader: { root: path.join(__dirname, "node_modules") },
         // don't transform node_modules folder (which don't need to be compiled)
         exclude: /node_modules/
       },
